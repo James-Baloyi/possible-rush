878a2e536210a36a9165e44ec524db389deb8975
James Baloyi <james_baloyi@yahoo.com>
2020-04-22 20:58:19 +0200

    Update

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@ self.addEventListener('fetch', event => {
 if(!navigator.onLine && caches.match(event.request)){
   event.respondWith(caches.match(event.request));
   }
-  if(navigator.connection.downLink < 0.5){
+  if(navigator.connection.downLink < 0.5 || !navigator.onLine){
     var cached = caches.match(event.request);
     event.respondWith(cached);
 }
